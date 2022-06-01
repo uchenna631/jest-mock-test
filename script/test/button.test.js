862559bc99fefad5a3b302ba71e28c4ca2310d78
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-06-01 22:49:43 +0000

    Test HTML file with jest

--- script/test/button.test.js
+++ script/test/button.test.js
@@ -4,13 +4,21 @@
 
 const buttonClick = require("../js/button");
 
-beforeAll(() => {
-    document.body.innerHTML = "<p id='par'></p>"
+beforeEach(() => {    
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM tests", () => {
     test("Expect p content to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You clicked");
+    })
+
+    test("Contains h1 element", () => {
+        expect(document.getElementsByTagName("h1").length).toEqual(1);
     })
 })
